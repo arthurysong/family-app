e80e739bcbbc07f7229ad3813e5181f94642ac2f
atribecalledarty <arth3rs0ng@gmail.com>
2019-07-15 23:09:50 -0700

    finished update action for board, you can now edit a header for family board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,6 +5,17 @@ class BoardsController < ApplicationController
     end
 
     def update
+        @board = Board.find(params[:id])
+        if @board.update(board_params)
+            redirect_to family_path(@board.family)
+        else
+            render 'boards/edit'
+        end
+    end
 
+    private
+
+    def board_params
+        params.require(:board).permit(:header)
     end
 end
