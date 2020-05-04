78f5964e370a14bfc7e55cb55333cbb22c58d54e
atribecalledarty <arth3rs0ng@gmail.com>
2020-05-04 15:03:06 -0700

    Changing app url to familytime2.herokuapp.com for OmniAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :github, '1b9c43f746de6b2e0856', '93e75a502bfdd5e57b34b6c6b89c0b605e44ab95', { 
-        :redirect_uri => "http://localhost:3000/auth/github/callback" 
+        :redirect_uri => "http://familytime2.herokuapp.com/auth/github/callback" 
     }
 end
